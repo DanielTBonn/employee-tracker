c7efc21662ea79f458b15504a663ba068e853eaf
Daniel Bonn <danieltbonn@gmail.com>
2023-08-28 20:00:44 -0500

    tested and succesfully created my first query by passing in information using the .execute method

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 // Import and require mysql2
 const mysql = require('mysql2');
-const schema = require('./assets/db/schema.sql');
-const seeds = require('./assets/db/seeds.sql');
 
 
 const PORT = process.env.PORT || 3001;
@@ -25,16 +23,11 @@ const db = mysql.createConnection(
   console.log(`Connected to the books_db database.`)
 );
 
-db.query(schema, function(err, results) {
-  console.log(results);
-});
 
-db.query(seeds, function(err, results) {
-  console.log(results);
-});
-
-db.query('SELECT * FROM department', function (err, results) {
-  console.log(results);
+db.execute('SELECT * FROM `department` WHERE `name` = ?', 
+  ['Engineering'], 
+  function (err, results) {
+    console.log(results);
 });
 
 // Default response for any other request (Not Found)
