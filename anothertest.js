ec0ad88fe9bbae309a363e5489309d391422de84
Daniel Bonn <danieltbonn@gmail.com>
2023-09-04 13:17:21 -0500

    added identical async func call with a different query and its not working

--- anothertest.js
+++ anothertest.js
@@ -8,10 +8,18 @@ const db = mysql.createConnection({
     database: 'department_db',
 }).promise();
 
+
 const employeeChoices = async () => {
     const employeeQuery = `SELECT CONCAT(first_name, ' ', last_name) AS name FROM employee;`;
     const employees = await db.query(employeeQuery);
     return employees[0];
+}
+
+const roleChoices = async () => {
+    const roleQuery = `SELECT title FROM role;`;
+    const roles = await db.query(roleQuery);
+    // console.log(roles);
+    return roles[0];
 }
 
 const questionPrompt = async() => {
@@ -22,11 +30,20 @@ const questionPrompt = async() => {
         message: 'What would you like to do? (use arrow keys)',
         choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit']
     },
+    // {
+    //     type: 'list',
+    //     name: 'employeeNames',
+    //     message: "What is the employee's name?",
+    //     choices: await employeeChoices(),
+    //     when: function(answers) {
+    //         return answers.menu === 'Update Employee Role'
+    //     }
+    // }, 
     {
         type: 'list',
-        name: 'employees',
-        message: "What is the employee's name?",
-        choices: await employeeChoices(),
+        name: 'roleName',
+        message: "What role is it?",
+        choices: await roleChoices(),
         when: function(answers) {
             return answers.menu === 'Update Employee Role'
         }
@@ -37,23 +54,24 @@ const questionPrompt = async() => {
 
 
 async function init() {
-    
-        // const { path } = await inquirer.prompt(([{
-        //     type: 'input',
-        //     name: 'message',
-        //     message: 'testing',
-        // }
-        // ]))
+
         let questions = await questionPrompt();
         const answers = await inquirer.prompt(questions)
-        console.log(answers);
+        const roles = await roleChoices();
+        console.log(roles)
+        if (answers.menu !== 'Quit') {
+            init();
+        } else {
+            process.exit();
+        }
     
 }
 
 // console.log(questions())
 const asyncCall = async() => {
-    const info = await employeeChoices();
+    const info = await roleChoices();
     console.log(info)
 }
 // asyncCall();
 init();
+
